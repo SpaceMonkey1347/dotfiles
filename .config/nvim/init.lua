26aae0b2595e6efe0e0762e9a4f2465b77a7a509
squid <SpaceMonkey1347@gmail.com>
2025-02-26 15:33:19 -0500

    part 2

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,5 @@
-require("core")
-local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
-vim.cmd.source(vimrc)
+require("domo.core.globals")
+require(CORE_PATH)
+require(CONFIG_PATH .. ".lazy")
+require(UTILS_PATH).load_mappings()
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("plugins")
-require("core.utils").load_mappings()
